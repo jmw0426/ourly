c456379503708f302541242341062151304804e9
jmw0426 <jmw0426@hotmail.com>
2021-02-03 15:28:18 -0600

    Added validations

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -3,4 +3,6 @@ class Schedule < ApplicationRecord
     belongs_to :user
     has_many :events
     has_many :shared_users, through: :events, source: user
+    validates :title, uniqueness: true, presence: true 
+    validates :month, presence: true
 end
