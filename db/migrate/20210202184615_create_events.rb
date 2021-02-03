cef0d6ea7316e35555b542ce5c8ed2db67ba343c
jmw0426 <jmw0426@hotmail.com>
2021-02-03 13:51:54 -0600

    Reconfigured Events Migration

--- db/migrate/20210202184615_create_events.rb
+++ db/migrate/20210202184615_create_events.rb
@@ -4,6 +4,8 @@ class CreateEvents < ActiveRecord::Migration[5.0]
       t.date :date
       t.time :time_start
       t.time :time_end
+      t.belongs_to :schedule, foreign_key: true
+      t.belongs_to :user, foreign_key: true
 
       t.timestamps
     end
