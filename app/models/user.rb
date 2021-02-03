63cb977f626fef2409bcebbf47c733b0478774d2
jmw0426 <jmw0426@hotmail.com>
2021-02-03 15:00:03 -0600

    Added username validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
     has_many :schedules 
     has_many :events, through: :schedules
     has_secure_password
+    validates :username, uniqueness: true, presence: true
 end
