e5a31bf0c143f755d96fe29a37bc63fc9bc6696f
jmw0426 <jmw0426@hotmail.com>
2021-02-03 15:22:04 -0600

    Added destroy method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,24 @@
 class SessionsController < ApplicationController
     def index
     end 
+
+    def new 
+    end 
+
+    def create
+        @user = User.find_by(username: params[:user][:username])
+
+      if @user && @user.authenticate(password: params[:user][:password])
+        session[:user_id] = @user.id
+        redirect_to user_path(@user)
+      else
+        flash[:error] = "Sorry, login info was incorrect. Please try again."
+        redirect_to login_path
+      end
+    end 
+
+    def destroy
+        session.delete(:user_id)
+        redirect_to '/'
+    end
 end
