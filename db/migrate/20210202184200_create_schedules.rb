f060df741e3afb1404af46f511d83ef56a335910
jmw0426 <jmw0426@hotmail.com>
2021-02-03 13:56:31 -0600

    Added title to Schedules Migration

--- db/migrate/20210202184200_create_schedules.rb
+++ db/migrate/20210202184200_create_schedules.rb
@@ -1,6 +1,7 @@
 class CreateSchedules < ActiveRecord::Migration[5.0]
   def change
     create_table :schedules do |t|
+      t.string :title
       t.string :month
       t.belongs_to :category, foreign_key: true
       t.belongs_to :user, foreign_key: true
