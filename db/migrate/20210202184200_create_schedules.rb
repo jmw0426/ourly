c417990dff190a60907a014e82a98be7f126601b
jmw0426 <jmw0426@hotmail.com>
2021-02-03 13:55:28 -0600

    Reconfigured Schedules Migration

--- db/migrate/20210202184200_create_schedules.rb
+++ db/migrate/20210202184200_create_schedules.rb
@@ -2,6 +2,8 @@ class CreateSchedules < ActiveRecord::Migration[5.0]
   def change
     create_table :schedules do |t|
       t.string :month
+      t.belongs_to :category, foreign_key: true
+      t.belongs_to :user, foreign_key: true
 
       t.timestamps
     end
