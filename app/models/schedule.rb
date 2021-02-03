2637b1761c6ba34249822d2b632f0b5b76fceed5
jmw0426 <jmw0426@hotmail.com>
2021-02-03 14:00:13 -0600

    Corrected has_many through formatting

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -2,5 +2,5 @@ class Schedule < ApplicationRecord
     belongs_to :category 
     belongs_to :user
     has_many :events
-    has_many :users through: :events
+    has_many :users, through: :events
 end
